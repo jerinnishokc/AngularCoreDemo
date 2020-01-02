b96238fc4064263d1bd0dcc261cf8e96369e9147
Jerin Nishok Christopher <jerinnishok.christopher@cognizant.com>
2020-01-02 19:01:17 +0530

    Adding two projects for practice.

---
+++ AngularCoreDemo_2.1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularCoreDemo2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public string Index() {
+			return "This is from the index function.";
+		}
+
+        public IActionResult Candy() {
+            return View();
+        }
+	}
+}
